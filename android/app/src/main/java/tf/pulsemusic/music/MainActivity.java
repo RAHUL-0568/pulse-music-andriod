17ef793f5ada21ec636131b7ba944d886fce40b1
Rahul <rahul@example.com>
2026-07-12 11:32:21 +0530

    Refactor: separate web-frontend and mobile-app

---
+++ android/app/src/main/java/tf/pulsemusic/music/MainActivity.java
@@ -0,0 +1,5 @@
+package tf.pulsemusic.music;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,16 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'tf.pulsemusic.music',
+    appName: 'Pulse Music Music',
+    webDir: '../web-frontend/dist',
+    assets: {
+        iconBackgroundColor: '#000000',
+        iconBackgroundColorDark: '#000000',
+        splashBackgroundColor: '#000000',
+        splashBackgroundColorDark: '#000000',
+    },
+};
+
+export default config;
+
